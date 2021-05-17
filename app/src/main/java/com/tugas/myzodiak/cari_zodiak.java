45880e07b3bc463d5d4a7a49537968c3e361ab61
martadana <komangmarta25@gmail.com>
2021-05-18 00:32:08 +0800

    update
    tambahan intent

---
+++ app/src/main/java/com/tugas/myzodiak/cari_zodiak.java
@@ -0,0 +1,14 @@
+package com.tugas.myzodiak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cari_zodiak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cari_zodiak);
+    }
+}
