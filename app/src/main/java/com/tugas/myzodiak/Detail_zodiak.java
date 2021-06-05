2a03839a5086d760e4ff36fb241eb8bccd7bf7eb
martadana <komangmarta25@gmail.com>
2021-06-05 16:23:01 +0800

    update

---
+++ app/src/main/java/com/tugas/myzodiak/Detail_zodiak.java
@@ -0,0 +1,41 @@
+package com.tugas.myzodiak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Detail_zodiak extends AppCompatActivity {
+    ImageView imageView3;
+    TextView tvd_zodiak, info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_zodiak);
+
+        imageView3 = findViewById(R.id.imageView3);
+        tvd_zodiak = findViewById(R.id.tvd_zodiak);
+        info = findViewById(R.id.detail);
+        getIncomingExtra();
+
+
+    }
+    private void getIncomingExtra(){
+        Intent mIntent = getIntent();
+
+        String namazodiak = mIntent.getStringExtra("nama_zodiak");
+        String moreinfo = mIntent.getStringExtra("info_zodiak");
+
+        setData(namazodiak, moreinfo);
+    }
+    private  void  setData( String namazodiak, String moreinfo){
+
+        tvd_zodiak.setText(namazodiak);
+        info.setText(moreinfo);
+
+    }
+
+}
--- app/src/main/java/com/tugas/myzodiak/MyAdapter.java
+++ app/src/main/java/com/tugas/myzodiak/MyAdapter.java
@@ -1,11 +1,13 @@
 package com.tugas.myzodiak;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,13 +34,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.my_row, parent, false);
         return new MyViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.judul.setText(data1[position]);
-        holder.deskripsi.setText(data2[position]);
         holder.imageView.setImageResource(images[position]);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(context, data1[position], Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(context, Detail_zodiak.class);
+                intent.putExtra("image_url", images[position]);
+                intent.putExtra("nama_zodiak", data1[position]);
+                intent.putExtra("info_zodiak", data2[position]);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -47,13 +60,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        public Object constraintLayout;
         TextView judul, deskripsi;
         ImageView imageView;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             judul = itemView.findViewById(R.id.judul);
-            deskripsi = itemView.findViewById(R.id.deskripsi);
             imageView = itemView.findViewById(R.id.imageView);
         }
     }
